ce4b496d4f13948bccee0a91eef465d63336b26d
tomer18-meet <redtomeradar@gmail.com>
2018-11-01 18:43:35 +0200

    s

--- databases.py
+++ databases.py
@@ -7,19 +7,31 @@ engine = create_engine('sqlite:///lecture.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
+MAX_PRICE = 300
 # Write your functions to interact with the database here :
 
-def create_product():
+def create_product(name, price, color, description):
+	
+	new_product = Product(product_name = name, price = price, color = color, description = description)
+	session.add(new_product)
+	session.commit()
+def update_product(name, price, color):
   #TODO: complete the functions (you will need to change the function's inputs)
-  pass
-
-def update_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+    if price > MAX_PRICE:
+  	    print("the price is too high!")
+    else:
+	    product = session.query(Product).filter_by(product_name = name).first()
+	    product.price = price
+	    product.color = color
 
 def delete_product(id):
-  pass
+ 	session.query(Product).filter_by(id = id).delete()
+ 	session.commit()
+
 
 def get_product(id):
-  pass
+	product = session.query(Product).filter_by(id = id).first()
+	return product
+# create_product("name", 2, "Red", "this is cool!")
+
+
--- model.py
+++ model.py
@@ -8,4 +8,12 @@ Base = declarative_base()
 # Write your classes here :
 class Product(Base):
     # TODO: complete this class
-    pass
+    __tablename__ = "products"
+    id = Column(Integer, primary_key = True)
+    product_name = Column(String)
+    price = Column(Integer)
+    color = Column(String)
+    description = Column(String)
+
+    def __repr__(self):
+    	return "name: " + self.product_name + "\nprice: " + str(self.price) + "\ncolor: " + self.color + "\ndescription: " + self.description
